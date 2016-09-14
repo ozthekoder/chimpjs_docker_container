00dc6d658c11dc99bd8cd743b7afe6951c4b1e8b
ozthekoder <oozdemir2704@gmail.com>
2016-09-14 17:19:33 -0400

    updated dummy test

--- spec/dummy.spec.js
+++ spec/dummy.spec.js
@@ -1,13 +1,24 @@
 
 describe('Chimp Mocha', function() {
-  describe('Page title', function () {
-    it('should be set by the Meteor method @watch', function () {
+  describe('Google Test', function () {
+    it('should be able to search stuff', function () {
       browser.url('http://www.google.com');
       expect(browser.getTitle()).to.equal('Google');
       browser.element('#lst-ib').keys('tayyip erdogan');
       browser.element('#lst-ib').keys('Enter');
       browser.waitForVisible('#rso > div:nth-child(1) > div:nth-child(1) > div > h3 > a', 50000);
       console.log(browser.getText('#rso > div:nth-child(1) > div:nth-child(1) > div > h3 > a'));
+    });
+  });
+
+  describe('Another Test', function () {
+    it('should be able to login to gitlab', function () {
+      browser.url('http://osmanozdem.ir');
+      browser.waitForVisible('#user_login', 50000)
+      browser.element('#user_login').keys('maestro');
+      browser.element('#user_password').keys('AllahVarGamYok19');
+      browser.waitForVisible('body > div.page-sidebar-collapsed.page-with-sidebar > div.content-wrapper > div.container-fluid.container-limited > div > div > div.projects-list-holder > ul > li:nth-child(1) > div.title > a > span > span.project-name.filter-title', 50000);
+      expect(browser.getText('body > div.page-sidebar-collapsed.page-with-sidebar > div.content-wrapper > div.container-fluid.container-limited > div > div > div.projects-list-holder > ul > li:nth-child(1) > div.title > a > span > span.project-name.filter-title'))).to.equal('generator-pieces');
     });
   });
 });
